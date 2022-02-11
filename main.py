e5b1ae39ae98451070cc3946775e9f35f3c7446d
UtkarshSrivastava-afk <utkarshs.ug20.me@nitp.ac.in>
2022-02-11 20:56:36 +0530

    complaintForm

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/jaankari"
+db = SQLAlchemy(app)
+
+
+class Complaint(db.Model):
+    name = db.Column(db.String(80), nullable=False)
+    city = db.Column(db.String(12), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    pincode = db.Column(db.String(15), nullable=False)
+    sno = db.Column(db.Integer, primary_key=True)
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/query", methods=['GET', 'POST'])
+def query():
+    if(request.method == 'POST'):
+        name = request.form.get('name')
+        city = request.form.get('city')
+        state = request.form.get('state')
+        pincode = request.form.get('pincode')
+        entry = Complaint(name=name, city=city, state=state, pincode=pincode)
+        db.session.add(entry)
+        db.session.commit()
+
+    return render_template("query.html")
 
 
 app.run(debug=True)
